5987b40770cc0ead49d93846e558e2da6a4d8d59
Moldrok <alessio.aquilano@outlook.it>
2023-08-13 23:26:05 +0200

    feat(manifest): add fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'mbt_minigames'
+description 'mbtech minigames'
+author 'Malibù Tech'
+version '1.0.0'
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/index.js',
+	'html/style.css',
+	'html/*.ogg'
+}
