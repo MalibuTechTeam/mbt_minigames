ab57315da9ee86d2509a096965c01bc9a88c7e13
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 16:20:11 +0000

    chore: bump manifest version to v0.0.91

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game         'gta5'
 
 name 'mbt_minigames'
 author 'Malibù Tech'
-version      '0.0.9'
+version      '0.0.91'
 description 'A collection of mini-games integrated into the Malibù Tech\'s scripts'
 
 client_scripts {
