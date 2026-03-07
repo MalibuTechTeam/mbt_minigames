a8acc0722cc8a8ebb7434fe35a62171d857441f0
Moldrok <alessio.aquilano@outlook.ti>
2026-03-07 01:05:46 +0100

    chore(config): balance difficulty profiles and fix animation prop orientations

--- config.lua
+++ config.lua
@@ -61,6 +61,81 @@ MBT.Locale = {
     }
 }
 
+-- Minigame Animation Configuration
+MBT.Animations = {
+    ['hacking'] = {
+        Dict = "anim@heists@ornate_bank@hack",
+        Prop = "hei_prop_hst_laptop",
+        Bag = "hei_p_m_bag_var22_arm_s",
+        OffsetZ = 0.9
+    },
+    ['wire_fix'] = {
+        ["Type"] = "Sequence",
+        ["Dict"] = "amb@world_human_welding@male@base",
+        ["Loop"] = "base",
+        ["EnterPropDelay"] = 0,
+        ["Props"] = {
+            {
+                Model = "prop_weld_torch",
+                Bone = 28422,
+                Offset = vector3(0.0, 0.0, 0.0),
+                Rot = vector3(0.0, 0.0, 0.0)
+            },
+            {
+                Model = "prop_welding_mask_01",
+                Bone = 31086,
+                Offset = vector3(0.12, 0.0, 0.0),
+                Rot = vector3(0.0, 90.0, 180.0)
+            }
+        },
+        ["Particles"] = {
+            {
+                PropIndex = 1,
+                Asset = "core",
+                Name = "ent_dst_electrical",
+                Offset = vector3(0.0, 0.35, 0.0),
+                Rot = vector3(0.0, 0.0, 0.0),
+                Scale = 2.0,
+                Pulse = 400
+            }
+        }
+    },
+    ['bolt_turn'] = {
+        ["Type"] = "Sequence",
+        ["PreEnterDict"] = "anim@heists@ornate_bank@hack",
+        ["PreEnter"] = "hack_enter",
+        ["PreEnterTime"] = 2000,
+        ["Dict"] = "anim@heists@fleeca_bank@drilling",
+        ["Enter"] = "drill_straight_start",
+        ["Loop"] = "drill_straight_idle",
+        ["EnterTime"] = 1200,
+        ["EnterPropDelay"] = 1500,
+        ["Props"] = {
+            {
+                Model = "hei_prop_heist_drill",
+                Bone = 28422,
+                Offset = vector3(0.0, 0.0, 0.0),
+                Rot = vector3(180.0, 0.0, 180.0)
+            }
+        }
+    },
+    ['code_match'] = {
+        ["Type"] = "Sequence",
+        ["Dict"] = "amb@code_human_in_bus_passenger_idles@female@tablet@base",
+        ["Loop"] = "base",
+        ["LoopFlag"] = 49,
+        ["EnterPropDelay"] = 0,
+        ["Props"] = {
+            {
+                Model = "prop_cs_tablet",
+                Bone = 60309, -- PH_L_Hand
+                Offset = vector3(0.03, 0.002, -0.0),
+                Rot = vector3(10.0, 160.0, 0.0)
+            }
+        }
+    }
+}
+
 -- Minigame Difficulty Configuration
 MBT.DefaultDifficulty = "Easy"
 MBT.Difficulties = {
@@ -71,19 +146,19 @@ MBT.Difficulties = {
             maxMistakes = 4,
         },
         ['wire_fix'] = {
-            time = 60,
+            time = 45,
             wireCount = 4,
             shuffleSpeed = 6000,
             maxMistakes = 4,
         },
         ['bolt_turn'] = {
-            time = 60,
-            boltCount = 4,
-            heatSpeed = 1.0,
+            time = 40,
+            boltCount = 3,
+            heatSpeed = 0.8,
             maxMistakes = 4,
         },
         ['code_match'] = {
-            time = 80,
+            time = 30,
             segmentCount = 4,
             shiftSpeed = 1200,
             maxMistakes = 4,
@@ -96,19 +171,19 @@ MBT.Difficulties = {
             maxMistakes = 3,
         },
         ['wire_fix'] = {
-            time = 30,
+            time = 55,
             wireCount = 5,
             shuffleSpeed = 4000,
             maxMistakes = 3,
         },
         ['bolt_turn'] = {
-            time = 45,
-            boltCount = 6,
-            heatSpeed = 1.5,
+            time = 50,
+            boltCount = 4,
+            heatSpeed = 1.2,
             maxMistakes = 3,
         },
         ['code_match'] = {
-            time = 45,
+            time = 35,
             segmentCount = 6,
             shiftSpeed = 900,
             maxMistakes = 3,
@@ -121,19 +196,19 @@ MBT.Difficulties = {
             maxMistakes = 2,
         },
         ['wire_fix'] = {
-            time = 15,
+            time = 70,
             wireCount = 6,
             shuffleSpeed = 2000,
             maxMistakes = 2,
         },
         ['bolt_turn'] = {
-            time = 25,
-            boltCount = 8,
-            heatSpeed = 2.0,
+            time = 65,
+            boltCount = 5,
+            heatSpeed = 1.6,
             maxMistakes = 2,
         },
         ['code_match'] = {
-            time = 25,
+            time = 30,
             segmentCount = 8,
             shiftSpeed = 600,
             maxMistakes = 2,
